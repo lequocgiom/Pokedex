49cf6dc3e60117565b28d5633698cc91ddeb7c65
lqtrung95 <44612846+lequocgiom@users.noreply.github.com>
2019-05-10 13:24:14 +0700

    basic collection view and background music

---
+++ Pokedex/View/PokeCell.swift
@@ -0,0 +1,31 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Le Trung on 5/10/19.
+//  Copyright © 2019 Le Trung. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
